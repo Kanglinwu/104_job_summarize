2f39d7518f8e2468b6cbf3c919f02f36dce9a22d
Kisl <kislxhotmail@gmail.com>
2020-08-05 22:35:25 +0800

    requests and soup - get issue with page for

--- app.py
+++ app.py
@@ -1,11 +1,51 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+from bs4 import BeautifulSoup
+
+import requests
+
 
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
-@app.route('/')
-def index():
-    return 'ok'
+@app.route('/<target>')
+def index(target):
+    # area = 6001006000 Hsinchu
+    templist = []
+    target_url = f'https://www.104.com.tw/jobs/search/?keyword={target}&area=6001006000&jobsource=2018indexpoc&ro=0'
+    # # using header as smart phone
+    # headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
+    # start request
+    origin_result = requests.get(target_url)
+
+    ## Using the bs4 to findall hyperlink
+    soup = BeautifulSoup(origin_result.content,'html.parser')
+    ## Search all a href
+    list_tags = soup.find_all('a')
+
+    ## for it
+    for hyperlink in list_tags:
+        ## check the link with job
+        if 'www.104.com.tw/job/' in hyperlink.get('href'):
+            ## append to list by dict type
+            templist.append(dict(company=hyperlink.text, hyperlink=hyperlink.get('href')))
+            # print(hyperlink.text, ' -- ', hyperlink.get('href'))
+        # print(hyperlink)
+    
+    print(templist)
+
+
+    # print(origin_result.status_code)
+    # print(origin_result.content)
+
+    # Save the result to file to see
+    fp = open('templates/result_104.html', 'a', encoding='utf-8')
+    fp.truncate(0)
+    fp.write(origin_result.text)
+    fp.close()
+    return render_template('result_104.html')
+
+    # return jsonify(templist)
 
 
 if __name__ == '__main__':
